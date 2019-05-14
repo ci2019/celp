3ad1a3418eacce9af65f887d824a2a90fdcece23
FurkanSimsir <furkansimsir@icloud.com>
2019-05-14 13:48:41 +0200

    test

--- recommender.py
+++ recommender.py
@@ -2,6 +2,7 @@ from data import CITIES, BUSINESSES, USERS, REVIEWS, TIPS, CHECKINS
 
 import random
 
+
 def recommend(user_id=None, business_id=None, city=None, n=10):
     """
     Returns n recommendations as a list of dicts.
@@ -15,6 +16,7 @@ def recommend(user_id=None, business_id=None, city=None, n=10):
             adress:str
         }
     """
+
     if not city:
         city = random.choice(CITIES)
     return random.sample(BUSINESSES[city], n)
